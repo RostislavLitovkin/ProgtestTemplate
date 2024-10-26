65af17de2d85d67680a2e73f33c09f8e414a577e
Rostislav Litovkin <rostislavlitovkin@gmail.com>
2024-10-26 12:55:18 +0200

    updated main.c

--- main.c
+++ main.c
@@ -2,16 +2,18 @@
 #include <stdbool.h>
 #include <float.h>
 #include <math.h>
+#include <limits.h> // INT_MAX, INT_MIN
 
 /// @brief Compare 2 doubles
-/// @param a 
-/// @param b 
-/// @return 
+/// @param a
+/// @param b
+/// @return true if the 2 doubles are equal
 bool doublesAreEqual(double a, double b)
 {
-    return (fabs(a - b) < (DBL_EPSILON * fabs(a + b)));
+  return (fabs(a - b) <= (DBL_EPSILON * fabs(a + b)));
 }
 
-int main() {
+int main()
+{
   return 0;
 }
