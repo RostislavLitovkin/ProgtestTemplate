96104b68400f703fe8efb1df6d0e35a160fe8410
Rostislav Litovkin <rostislavlitovkin@gmail.com>
2024-11-03 11:26:45 +0100

    printl

--- main.c
+++ main.c
@@ -2,6 +2,29 @@
 #include <stdbool.h>
 #include <float.h>
 #include <math.h>
+#include <limits.h> // INT_MAX, INT_MIN
+#include <stdarg.h>
+
+typedef unsigned int uint;
+typedef unsigned long ulong;
+typedef unsigned char uchar;
+
+/// @brief Custom printf fucntion that always prints a new line at the end.
+/// @param format
+/// @param
+void printl(const char *format, ...)
+{
+  va_list args;
+  va_start(args, format);
+
+  // Use vprintf to handle the variable arguments
+  vprintf(format, args);
+
+  // Print a newline character at the end
+  printf("\n");
+
+  va_end(args);
+}
 
 /// @brief Compare 2 doubles
 /// @param a 
